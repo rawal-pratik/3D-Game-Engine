ea0a8f97b49efdcf71fbfeb9d1a408968d7d20c5
rawal-pratik <pratikrawal40@gmail.com>
2024-10-05 22:19:42 +0530

    Added Vertex and Mesh Classes. (Not working as intended)

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -4,8 +4,14 @@ import org.lwjgl.input.Keyboard;
 
 public class Game {
 	
+	private Mesh mesh;
+	
 	public Game() {
-		
+		mesh = new Mesh();
+		Vertex[] data = new Vertex[] {new Vertex(new Vector3f(-1,-1,0)),
+										new Vertex(new Vector3f(-1, 1, 0)),
+										new Vertex(new Vector3f(0, 1, 0))};
+		mesh.addVertices(data);
 	}
 	
 	public void input() {
