c1c15c5b469f2e100ff5579edea145cdfe9ddb3a
rawal-pratik <pratikrawal40@gmail.com>
2024-10-23 22:05:15 +0530

    Implemented shaders to generate color as per position.

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -5,13 +5,18 @@ import org.lwjgl.input.Keyboard;
 public class Game {
 	
 	private Mesh mesh;
+	private Shader shader;
 	
 	public Game() {
 		mesh = new Mesh();
+		shader = new Shader();
 		Vertex[] data = new Vertex[] {new Vertex(new Vector3f(-1,-1,0)),
 										new Vertex(new Vector3f(0, 1, 0)),
 										new Vertex(new Vector3f(1, -1, 0))};
 		mesh.addVertices(data);
+		shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vs"));
+		shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.fs"));
+		shader.compileShader();
 	}
 	
 	public void input() {
@@ -34,6 +39,7 @@ public class Game {
 	
 	public void render() {
 		mesh.draw();
+		shader.bind();
 	}
 	
 }
